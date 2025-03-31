9764124a38a0c55013fe97e1ce9f0e075b65e05b
sona772004 <your.email@example.com>
2025-03-31 19:54:38 +0530

    Initial commit

---
+++ preprocessed.py
@@ -0,0 +1,87 @@
+import json
+import re
+from llm_helper import llm
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+
+
+def clean_text(text):
+    """Removes surrogate Unicode characters and ensures safe encoding."""
+    text = re.sub(r'[\uD800-\uDFFF]', '', text)  # Remove surrogate pairs
+    return text.encode("utf-8", "ignore").decode("utf-8")  # Ignore bad characters
+
+
+def process_posts(raw_file_path, processed_file_path=None):
+    with open(raw_file_path, encoding='utf-8') as file:
+        posts = json.load(file)
+        enriched_posts = []
+        for post in posts:
+            post['text'] = clean_text(post['text'])  # 🛠 Apply text cleaning
+            metadata = extract_metadata(post['text'])
+            post_with_metadata = post | metadata
+            enriched_posts.append(post_with_metadata)
+
+    unified_tags = get_unified_tags(enriched_posts)
+    for post in enriched_posts:
+        current_tags = post['tags']
+        new_tags = {unified_tags.get(tag, tag) for tag in current_tags}  # 🛠 Handle missing tags
+        post['tags'] = list(new_tags)
+
+    with open(processed_file_path, "w", encoding="utf-8") as outfile:
+        json.dump(enriched_posts, outfile, indent=4, ensure_ascii=False)  # 🛠 Ensure UTF-8 writing
+
+
+def extract_metadata(post):
+    template = '''
+    You are given a LinkedIn post. You need to extract number of lines, language of the post, and tags.
+    1. Return a valid JSON. No preamble. 
+    2. JSON object should have exactly three keys: line_count, language, and tags. 
+    3. tags is an array of text tags. Extract a maximum of two tags.
+    4. Language should be English or Hinglish (Hinglish means Hindi + English)
+
+    Here is the actual post on which you need to perform this task:  
+    {post}
+    '''
+
+    pt = PromptTemplate.from_template(template)
+    chain = pt | llm
+    response = chain.invoke(input={"post": post})
+
+    try:
+        json_parser = JsonOutputParser()
+        res = json_parser.parse(response.content)
+    except OutputParserException:
+        raise OutputParserException("Context too big. Unable to parse jobs.")
+    return res
+
+
+def get_unified_tags(posts_with_metadata):
+    unique_tags = set()
+    for post in posts_with_metadata:
+        unique_tags.update(post['tags'])
+
+    unique_tags_list = ','.join(unique_tags)
+
+    template = '''I will give you a list of tags. You need to unify them based on the following:
+    1. Merge similar tags into one. 
+       Example: "Jobseekers", "Job Hunting" → "Job Search". 
+    2. Use Title Case for all tags.
+    3. Output a JSON mapping of original tags to unified tags.
+
+    Here is the list of tags: 
+    {tags}
+    '''
+    pt = PromptTemplate.from_template(template)
+    chain = pt | llm
+    response = chain.invoke(input={"tags": str(unique_tags_list)})
+    try:
+        json_parser = JsonOutputParser()
+        res = json_parser.parse(response.content)
+    except OutputParserException:
+        raise OutputParserException("Context too big. Unable to parse jobs.")
+    return res
+
+
+if __name__ == "__main__":
+    process_posts("data/raw_post.json", "data/processed_posts.json")
